4f48de6ca72b0bcc2b65d33e2684154b14f746a0
Dilson Pereira <dilsonpereira231@gmail.com>
2025-05-23 22:33:22 -0300

    Primeiro commit - Projeto Loja de Açaí

---
+++ Windows_Desktop/CURSOS/ALGORITIMO/aula 5/exercicio 1 materia/exercicio1.py
@@ -0,0 +1,11 @@
+# escreva uma rotina que crie uma borad ao redor de uma palavra, 
+# para dedstaca-la como dendo um titulo,
+# a rotina deve receber um parametro a palvra
+# a ser destaca.
+# o tamanha da caixa de texto devera ser adaptavel,
+# de cacordo com o tamanho. por exemplo:
+
+#vinicius = ola
+
+borda('ola,mundo!')
+borda('lógica de programação e algoritimo')
